35774a7ec33cbfd6dd9bdc522e3a063fb91fd406
Daniel Sochor <danielsochor@gmail.com>
2019-07-26 07:05:10 -0500

    adjustment of card counter

--- src/App.js
+++ src/App.js
@@ -9,19 +9,39 @@ class App extends Component {
   state = {
     cards,
     score: 0,
-    highscore: 0
+    highscore: 0,
+    clickCount: 0,
   }
 
   clickCounter = id => {
     console.log(id);
-    this.state.cards.find((id) => {
-      console.log("starting count: " + cards[id]);
-      cards[id].count = cards[id] + 1;
-      console.log("ending count: " + cards[id].count);
-      return cards[id].count
-    });
-    //this.state.cards.sort(() => Math.random() - 0.5)
+    console.log(this.state.card)
+//loop through cards
+//then check click matches
+//retunr statements 
+
+
+
+      this.state.cards.find((card) => {
+        if(c)
+        if (card(id).clickCount < 1){
+
+
+
+        console.log('starting count: ' + cards[id]);
+        cards[id].count = cards[id] + 1;
+        console.log('ending count: ' + cards[id].count);
+        return cards[id].count
+            //this.state.cards.sort(() => Math.random() - 0.5)
+      });
+    } else {
+      console.log('this has already been picked')
+    }
   }
+  //this is reference ot the class we're in
+  //we're looping through all the cards in the current state
+  //finding the card that matches the id from that which was clicked
+
 
 
   render() {
