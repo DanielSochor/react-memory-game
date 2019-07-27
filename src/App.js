e12849f383c0ebba695510ff1b76fb2a0049ce0b
Daniel Sochor <danielsochor@gmail.com>
2019-07-27 15:42:59 -0500

    card click tracking works

--- src/App.js
+++ src/App.js
@@ -13,47 +13,28 @@ class App extends Component {
   }
 
   shuffleCards = () => {
-    //console.log('pre shuffled cards: ');
-    //console.log(cards);
-    //console.log('suffle cards');
-    //var newCards = this.state.cards.sort(() => Math.random() - 0.5);
-    //console.log('shuffled array is: ' + newCards)
     return this.setState([
       this.cards = this.state.cards.sort(() => Math.random() - 0.5)
     ]);
-    //console.log('post shuffled cards: ');
-    //console.log(cards);
-    //this.displayCards(newCards);
   }
 
   handleClick = (clickedCardId) => {
-    //var image_id = id
     console.log('clicked id: ' + clickedCardId)
-    console.log('clicked card: ' + cards[clickedCardId].id)
+
     for (var i = 0; i < cards.length; i++) {
-      //console.log(cards[i].id, cards[i].clicked)
-      if (cards[clickedCardId].id === clickedCardId) {
-        console.log('clicked card is: ' + cards[clickedCardId].id)
+      if (cards[i].id === clickedCardId) {
+        var card = cards[i];
       }
     }
-    if (cards[clickedCardId].clicked) {
+    if (card.clicked) {
       console.log('has been clicked')
       this.shuffleCards()
     } else {
       console.log('has not been clicked')
-      cards[clickedCardId].clicked = true
+      card.clicked = true
       this.shuffleCards()
     }
-    //this.displayCards()
   }
-
-  //displayCards = (Cards) => {
-  //console.log(Cards);
-  //for( var i=0; i<newCards.length; i++){
-  //   console.log(cards[i].id, newCards[i].clicked)
-  //}
-  //}
-  //CARS ARE DISPLAYING CORRECTLY
 
   render() {
     return (
